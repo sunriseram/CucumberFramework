444e9ac7b752782ecc1c522049f2805de5f4b33e
[udhayarani Nallathambi] <[sunriseram2002@gmail.com]>
2022-02-20 13:06:28 -0500

    POM.xml Apitesting dependency added

--- src/test/java/step_definitions/PetStoreSteps.java
+++ src/test/java/step_definitions/PetStoreSteps.java
@@ -1,16 +1,29 @@
 package step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import java.io.File;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
-
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class PetStoreSteps {
 	
 	Response response;
+	private String requestURL;
+	File requestBody;
+	int petId;
+	
+	
+	// in RestAssured, we can do the data validation in many different ways
+	// we can use JUnit or TestNG assertions as we learned
+	// and also we can use build in RestAssured Assertions
+	
 	
 	@When("I am able to perform Post request to create a pet with valid request body")
 	public void i_am_able_to_perform_post_request_to_create_a_pet_with_valid_request_body() {
@@ -59,8 +72,97 @@ public class PetStoreSteps {
 		// Assert.assertEquals(id, 123678);
 		Assert.assertEquals(response.path("name"), "Husky");
 		Assert.assertEquals(response.path("status"), "sold");
+		
+	}
+		// post to add a pet Scenario #Starts
+		
+		@Given("I have the POST request URL and valid request body")
+		public void i_have_the_post_request_url_and_valid_request_body() {
+			requestURL = "https://petstore.swagger.io/v2/pet";
+			requestBody = new File("./src/test/resources/jsonFiles/addAPet.json");
+		}
+		@When("I perform POST request to URL with request body")
+		public void i_perform_post_request_to_url_with_request_body() {
+			response = given().contentType(ContentType.JSON).accept(ContentType.JSON).body(requestBody)
+		    .when().post(requestURL);
+		}
+		@Then("Response status code should be {int}")
+		public void response_status_code_should_be(Integer expectedStatusCode) {
+		    response.then().assertThat().statusCode(expectedStatusCode);
+		}
+		@Then("Content type is {string}")
+		public void content_type_is(String expectedContentType) {
+		    response.then().assertThat().contentType(expectedContentType);
+		}
+		@Then("Response body match the request body")
+		public void response_body_match_the_request_body() {
+			petId = response.jsonPath().getInt("id");
+			// there is something called chain validation in restAssured
+		   response.then()
+		   .assertThat().statusCode(200)
+		   .and().assertThat().contentType("application/json")
+		   .and().assertThat().body("id", equalTo(667788))
+		   .and().assertThat().body("name", equalTo("Fluffy"))
+		   .and().assertThat().body("status", equalTo("available"))
+		   .and().assertThat().body("category.name", equalTo("Cat"))
+		   .and().assertThat().body("tags[0].name", equalTo("dog"));
+		}
+		
+		
+		
+		// invalid post request body scenario #Starts 
+		@Given("I have the POST request URL and invalid request body")
+		public void i_have_the_post_request_url_and_invalid_request_body() {
+			requestURL = "https://petstore.swagger.io/v2/pet";
+			requestBody = new File("./src/test/resources/jsonFiles/invalidRequestBody.json");
+		}
+		@Then("Response status code should be {int} invalid input")
+		public void response_status_code_should_be_invalid_input(Integer expectedStatusCode) {
+			response.then().assertThat().statusCode(expectedStatusCode);
+		}
+		@Then("message should be {string}")
+		public void message_should_be(String expectedMessageValue) {
+			response.prettyPrint();
+		    response.then().assertThat().body("message", equalTo(expectedMessageValue));
+		}
+		
+		
+		// get request by status scenario outline #Starts
+		
+		@Given("valid endpoint exist")
+		public void valid_endpoint_exist() {
+			requestURL = "https://petstore.swagger.io/v2/pet/findByStatus";
+		}
+		@When("I send a GET request by status {string} to valid endpoint")
+		public void i_send_a_get_request_by_status_to_valid_endpoint(String actualStatus) {
+		   response = given().accept(ContentType.JSON).queryParam("status", actualStatus)
+		   .when().get(requestURL);
+		   
+		   response.prettyPrint();
+		   
+		}
+		@Then("Content type should be {string}")
+		public void content_type_should_be(String actualContentType) {
+		    response.then().assertThat().contentType(actualContentType);
+		}
+		
+		
+		// invalid URL get request scenario #Starts
+		
+		@Given("Invalid endpoint exist")
+		public void invalid_endpoint_exist() {
+			// this is invalid URL
+		    requestURL = "https://petstore.swagger.io/v2/pet/findBy";
+		}
+		@When("I send a GET request by status {string} to invalid endpoint")
+		public void i_send_a_get_request_by_to_valid_endpoint(String expectedStatus) {
+		   response = given().accept(ContentType.JSON).queryParam("status", expectedStatus)
+		   .when().get(requestURL);
+		}
+		//get by Pet ID
+		
 	}
 
-}
+
 
 
--- target/test-classes/features/PetStore.feature
+++ target/test-classes/features/PetStore.feature
@@ -5,3 +5,74 @@ Feature: As a user I am able to perform HTTP request
 @CreateAPet   
 Scenario: I am able to create a Pet using Post request
 When I am able to perform Post request to create a pet with valid request body
+
+
+@PostToAddPet
+  Scenario: As a user, I should be able to perform POST request to add new pet to store
+    Given I have the POST request URL and valid request body
+    When I perform POST request to URL with request body
+    Then Response status code should be 200
+    And Content type is "application/json"
+    And Response body match the request body
+
+  @invalidPostRequest
+  Scenario: As a user, I should not be able to perform POST request to invalid data structure
+    Given I have the POST request URL and invalid request body
+    When I perform POST request to URL with request body
+    Then Response status code should be 500 invalid input
+    And Content type is "application/json"
+    And message should be "something bad happened"
+
+  @getRequestByStatus
+  Scenario Outline: Find a pet by status
+    Given valid endpoint exist
+    When I send a GET request by status "<status>" to valid endpoint
+    Then Response status code should be 200
+    And Content type should be "application/json"
+
+    Examples: 
+      | status    |
+      | available |
+      | pending   |
+      | sold      |
+
+  @invalid_URL_Get
+  Scenario: Find a pet by invalid url using status available
+    Given Invalid endpoint exist
+    When I send a GET request by status "available" to invalid endpoint
+    Then Response status code should be 404
+    And Content type should be "application/json"
+
+  @getByPetID
+  Scenario: As a user, I should be able to perform GET request to find a pet by id
+    Given I have the GET request URL
+    When I perform GET request to URL with pet id 6622456
+    Then Response status code should be 200
+    And Content type should be "application/json"
+    And Pet id is 6622456, pet name is "doggie" status is "available"
+    And Category id is 22232, category name is "Booboo"
+    And Tags id is 22232, and tags name is "dog"
+
+  @getAPetByInvalidID
+  Scenario: As a user, I should be able to perform GET request to find a pet by invalid id
+    Given I have the GET request URL
+    When I perform GET request to URL with invalid id 66224
+    Then Response status code should be 404
+    And Content type should be "application/json"
+    And message should be "Pet not found"
+
+  @deleteByID
+  Scenario: As a user, I am able to delete a pet
+    Given I have the Delete request URL
+    When I perform DELETE request to URL with valid id 662
+    Then Response status code should be 200
+    And Content type should be "application/json"
+    And message should be "662"
+
+  @updateAPet
+  Scenario: As a user, I am able to update a pet
+    Given I have the Update request URL
+    When I perform UPDATE request to update Name to existing Pet
+    Then Response status code should be 200
+    And Content type should be "application/json"
+    And Id is 667788 and is "Meow"
